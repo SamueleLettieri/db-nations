db7566145400b72273918358041492b3f6963f8a
SamueleLettieri <samuele2073@gmail.com>
2023-02-08 16:42:48 +0100

    Fine esercizio

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,22 +12,29 @@ public class Main {
 		String url = "jdbc:mysql://localhost:3306/ls_dbNations";
 		String user = "root";
 		String password = "root";
+		Scanner s = new Scanner(System.in);
 		
 		
+		System.out.print("Inserisci il nome della nazione da cercare: ");
+		String countri = s.nextLine();
 		
+		s.close();
 		try(Connection con = DriverManager.getConnection(url, user, password)) {
 			
 			String sql = "SELECT c.NAME, c.COUNTRY_ID, r.NAME, c2.NAME" 
-							+" FROM COUNTRIES C "
-							+" INNER JOIN REGIONS R "
-							+" ON c.REGION_ID = r.REGION_ID "
-							+" INNER JOIN CONTINENTS C2 "
-							+" ON r.CONTINENT_ID = c2.CONTINENT_ID "
-							+" ORDER BY r.NAME  DESC ";
+							+" FROM COUNTRIES C"
+							+" INNER JOIN REGIONS R"
+							+" ON c.REGION_ID = r.REGION_ID"
+							+" INNER JOIN CONTINENTS C2"
+							+" ON r.CONTINENT_ID = c2.CONTINENT_ID"
+							+" WHERE c.name LIKE ?"
+							+" ORDER BY r.NAME  DESC";
 			
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
 				
-				try(ResultSet rs = ps.executeQuery(sql)) {
+				ps.setString(1, '%'+ countri +'%');
+				
+				try(ResultSet rs = ps.executeQuery()) {
 					
 					while (rs.next()) {
 						
